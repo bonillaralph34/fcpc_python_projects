a568ab23e66f81de590754a0867c0e5d1a280173
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-11-27 07:50:00 +0800

    Create  Sample_Bonilla.py

---
+++ Sample_Bonilla.py
@@ -0,0 +1,50 @@
+# Declaring and initializing variables in Python
+
+#Strings
+arteezy = "Evil Geniuses"
+noone = "Virtus Pro"
+kuku = "TNC"
+fata = "Secret"
+
+#Numerical values
+number_of_teams = 5
+number_of_players = 50
+matches = 10
+
+print(arteezy)
+print(noone)
+print(kuku)
+print(fata)
+
+print(number_of_teams)
+print(number_of_players)
+print(matches)
+
+# Arithmetic
+addition = 50 + 50
+subtraction = 2 - 2
+division = 1000 / 2
+multlipication = 50 * 2
+modulus = 100 % 2
+
+print(addition)
+print(subtraction)
+print(division)
+print(multlipication)
+print(modulus)
+
+
+if addition > division:
+    print("Welcome")
+    print("back")
+    print("tutut")
+elif division > subtraction:
+    print("Please")
+    print("stay")
+    print("with")
+    print("me")
+
+else:
+    print("Go")
+    print("away")
+    print("tutut")
