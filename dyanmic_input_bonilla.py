17c233cf5c0cf8e40542ca09b61c3536a69fa4fa
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-11-27 07:47:45 +0800

    test commit file

---
+++ dyanmic_input_bonilla.py
@@ -0,0 +1,13 @@
+#accept input form user
+name = input("Enter your name:")
+print("Your name is: " + name)
+
+#accepts numerical input
+fnum = int(input("Enter first number:"))
+snum= int(input("Enter second number:"))
+tnum= int(input("Enter third number:"))
+
+
+to_int_value = int("50")
+print(fnum + snum + tnum)
+print(to_int_value **3)
