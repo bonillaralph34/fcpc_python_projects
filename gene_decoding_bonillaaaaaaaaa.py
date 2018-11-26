ae21d4609ef2b6b8d54cd2bf261f769e27c4a02e
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-11-27 07:46:03 +0800

    test commit file

---
+++ gene_decoding_bonillaaaaaaaaa.py
@@ -0,0 +1,62 @@
+name = input("Enter your name:")
+name_changer_2 = ""
+
+for x in range(len(name)):
+    char = name[x]
+
+    if char == "A":
+        name_changer_2 += "B"
+    elif char == "B":
+        name_changer_2 += "C"
+    elif char == "C":
+        name_changer_2 += "D"
+    elif char == "D":
+        name_changer_2 += "E"
+    elif char == "E":
+        name_changer_2 += "F"
+    elif char == "F":
+        name_changer_2 += "G"
+    elif char == "G":
+        name_changer_2 += "H"
+    elif char == "H":
+        name_changer_2 += "I"
+    elif char == "I":
+        name_changer_2 += "J"
+    elif char == "J":
+        name_changer_2 += "K"
+    elif char == "K":
+        name_changer_2 += "L"
+    elif char == "L":
+        name_changer_2 += "M"
+    elif char == "M":
+        name_changer_2 += "N"
+    elif char == "N":
+        name_changer_2 += "O"
+    elif char == "O":
+        name_changer_2 += "P"
+    elif char == "P":
+        name_changer_2 += "Q"
+    elif char == "Q":
+        name_changer_2 += "R"
+    elif char == "R":
+        name_changer_2 += "S"
+    elif char == "S":
+        name_changer_2 += "T"
+    elif char == "T":
+        name_changer_2 += "U"
+    elif char == "U":
+        name_changer_2 += "V"
+    elif char == "V":
+        name_changer_2 += "W"
+    elif char == "W":
+        name_changer_2 += "X"
+    elif char == "X":
+        name_changer_2 += "Y"
+    elif char == "Y":
+        name_changer_2 += "Z"
+    elif char == "Z":
+        name_changer_2 += "Z"
+    else:
+        name_changer_2 += char
+
+print(name_changer_2)
