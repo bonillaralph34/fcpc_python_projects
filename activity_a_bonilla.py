636c2c2ab4a39b1e60d24b5c16b120e513df191c
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-11-27 11:13:39 +0800

    Create activity_a_bonilla.py

---
+++ activity_a_bonilla.py
@@ -0,0 +1,16 @@
+
+for x in range (1,17,3):
+    print(x,end="\t")
+
+print("\n")
+for y in range (100,78,-7):
+    print(y,end="\t")
+
+print("\n")
+count = ("1 O")
+for z in range (1,5):
+
+    if z % 3 == 0:
+        print("1",end="\t")
+    else:
+        print("O",end="\t"
