377dc2a6f33f2a3ad89cb06f9ace1ff880cea103
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-11-27 10:01:36 +0800

    Create list_bonilla.py

---
+++ list_bonilla.py
@@ -0,0 +1,18 @@
+#Strings
+mga_pogi = ["Ralph" , "Clerigo" , "Cruz" , "Andresa", "Brigatay"]
+mga_porsyento = ["100%", "90%" , "100%" , "90%" , "50%" , "100%"]
+mga_pagkasunodsunod = ["1" , "2" , "3" , "4" , "5"]
+
+for x in range(len(mga_pogi)):
+    #print(mga_pogi[x])
+
+for y in range(len(mga_porsyento)):
+    print(mga_porsyento[y])
+
+for z in range(len(mga_pagkasunodsunod)):
+    print(mga_pagkasunodsunod[z])
+
+#numbers of char
+print(len(mga_pogi))
+print(len(mga_porsyento))
+print(len(mga_pagkasunodsunod))
