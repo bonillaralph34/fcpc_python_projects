1b915021e0df05c04c2d01279cc7fac14301e44d
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-12-11 11:35:33 +0800

    Create list_sample_bonilla.py

---
+++ list_sample_bonilla.py
@@ -0,0 +1,16 @@
+
+list1 = ["Arteezy","Noone","Sumail","Crit"]
+for i in range (len(list1)):
+    print(list1[i])
+print("\n")
+list2 = ["A","N","S","C"]
+for z in range (len(list2)):
+    print(list2[z])
+print("\n")
+list3 = [1,3,2,4]
+for x in range(len(list3)):
+    print(list3[x])
+print("\n")
+list4 = ["EG","VP","TNC","MNSK"]
+for y in range(len(list4)):
+    print(list4[y])
