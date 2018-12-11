8404b2729f5c4ea3f36a96ec5f0608a1355f09ee
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-12-11 09:25:31 +0800

    Rename activity_a_bonilla.py to activity_A_bonilla.py

---
+++ activity_A_bonilla.py
@@ -0,0 +1,26 @@
+#var
+ralph = 90
+clerigo = 95
+balbin = 100
+andresa = 20
+print(ralph)
+print(clerigo)
+print(balbin)
+print(andresa)
+
+#operators
+value1 = 90
+value2 = 20
+print(value1-value2)
+
+#conditionals
+if(andresa <= 30):
+    print ("bagsak")
+elif(andresa >= 10):
+    print("pwede na")
+else:
+    print ("matalino")
+
+#loops
+for x in range(1,5,1):
+    print("x")
