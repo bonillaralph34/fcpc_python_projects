cd1b5d4dcaf4fc39783c4d240afdd0418fba3d4d
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-12-18 11:27:06 +0800

    Create activity_b_list_bonilla.py

---
+++ activity_b_list_bonilla.py
@@ -0,0 +1,17 @@
+list_products = ["Maxx", "Hansel", "Clover", "Safeguard","Zonrox"]
+list_price = [1, 6, 8, 12, 15]
+
+for x in range(len(list_products)):
+    print(list_products[0],"-",list_price[0]) 
+    
+for y in range(len(list_products)):
+    print(list_products[1],"-",list_price[1])
+
+for z in range(len(list_products)):
+    print(list_products[2],"-",list_price[2])
+
+for a in range(len(list_products)):
+    print(list_products[3],"-",list_price[3])
+    
+for b in range(len(list_products)):
+    print(list_products[4],"-",list_price[4])
