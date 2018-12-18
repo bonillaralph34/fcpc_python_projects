74a4bcf97f75e3e9aec121d03893ea4cc15b846a
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2018-12-18 11:26:17 +0800

    Update recursion_roman_bonilla.py

--- recursion_roman_bonilla.py
+++ recursion_roman_bonilla.py
@@ -1,36 +1,143 @@
 def toRomanNum(x):
-    if x <= 200:
-        if x == 200:
-            print("CC", end="")
-        elif x < 200 and x > 100:
-            print("C", end="")
-            a = x - 100
+    if x<=200:
+        if x==200:
+            print("CC",end='')
+        elif x<200 and x>199:
+            print("CXCIX",end='')
+            v=x-199
+            toRomanNum(v)
+        elif x==199:
+            print("CXCIX",end='')
+        elif x<199 and x>190:
+            print("CXC",end='')
+            c=x-190
+            toRomanNum(c)
+        elif x==190:
+            print("CXC",end='')
+        elif x<190 and x>180:
+            print("CLXXX",end='')
+            z=x-180
+            toRomanNum(z)
+        elif x==180:
+            print("CLXXX",end='')
+        elif x<180 and x>170:
+            print("CLXX",end='')
+            l=x-170
+            toRomanNum(l)
+        elif x==170:
+            print("CLXX",end='')
+        elif x<170 and x>160:
+            print("CLX",end='')
+            k=x-160
+            toRomanNum(k)
+        elif x==160:
+            print("CLX",end='')
+        elif x<160 and x>150:
+            print("CL",end='')
+            j=x-150
+            toRomanNum(j)
+        elif x==150:
+            print("CL",end='')
+        elif x<150 and x>140:
+            print("CXL",end='')
+            h=x-140
+            toRomanNum(h)
+        elif x==140:
+            print("CXL",end='')
+        elif x<140 and x>120:
+            print("CXX",end='')
+            g=x-120
+            toRomanNum(g)
+        elif x==120:
+            print("CXX",end='')
+        elif x<120 and x>110: 
+            print("CX",end='') 
+            f=x-110
+            toRomanNum(f)
+        elif x==110:
+            print("CX",end='')
+        elif x==109: 
+            print("CIX",end='')
+        elif x<109 and x>105:
+            print("CV",end='') 
+            d=x-105 
+            toRomanNum(d)
+        elif x==105:
+            print("CV",end='')
+        elif x==104:
+            print("CIV",end='')
+        elif x<104 and x>100: 
+            print("C",end='') 
+            s=x-100
+            toRomanNum(s)
+        elif x==100:
+            print("C",end='')
+        elif x<100 and x>99:
+            print("XCIX",end='')
+            e=x-99
+            toRomanNum(e)
+        elif x==99:
+            print("XCIX",end='')
+        elif x<99 and x>90:
+            print("XC",end='')
+            r=x-90
+            toRomanNum(r)
+        elif x==90:
+            print("XC",end='')
+        elif x<90 and x>80:
+            print("LXXX",end='')
+            i=x-80
+            toRomanNum(i)
+        elif x==80:
+            print("LXXX",end='')
+        elif x<80 and x>70:
+            print("LXX",end='')
+            u=x-70
+            toRomanNum(u)
+        elif x==70:
+            print("LXX",end='')
+        elif x<70 and x>60:
+            print("XL",end='')
+            y=x-60
+            toRomanNum(y)
+        elif x==60:
+            print("LX",end='')
+        elif x<60 and x>50:
+            print("XL",end='')
+            t=x-50
+            toRomanNum(t)
+        elif x==50:
+            print("L",end='')
+        elif x<50 and x>40:
+            print("XL",end='')
+            w=x-40
+            toRomanNum(w)
+        elif x==40:
+            print("XL",end='')
+        elif x<40 and x>20:
+            print("XXX",end='')
+            q=x-20
+            toRomanNum(q)
+        elif x==20:
+            print("XX",end='')
+        elif x<20 and x>10: 
+            print("X",end='') 
+            y=x-10
+            toRomanNum(y)
+        elif x==10:
+            print("X",end='')
+        elif x==9: 
+            print("IX",end='')
+        elif x<9 and x>5:
+            print("V",end='') 
+            a=x-5 
             toRomanNum(a)
-        elif x == 99:
-            print("IC", end="")
-        elif x <= 98 and x >= 50:
-            print("L", end="")
-            a = x - 50
-            toRomanNum(a)
-        elif x == 49:
-            print("IL", end="")
-        elif x <= 48 and x >= 40:
-            print("XL", end="")
-            a = x - 40
-            toRomanNum(a)
-        elif x <= 39 and x >= 10:
-            print("X", end="")
-            a = x - 10
-            toRomanNum(a)
-        elif x <= 9 and x >= 5:
-            print("V", end="")
-            a = x - 5
-            toRomanNum(a)
-        elif x < 5 and x > 0:
-            print("I", end="")
-            a = x - 1
-            toRomanNum(a)
+        elif x==4:
+            print("IV",end='')
+        elif x<5 and x>0: 
+            print("I",end='') 
+            b=x-1
+            toRomanNum(b)
         else:
             print()
-
-toRomanNum(80)
+toRomanNum(180)
