8dea0f4c50d2c69662f2603e5a92fb93e2666bee
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2019-01-21 19:02:08 -0800

    Create splice_bonilla.py

---
+++ splice_bonilla.py
@@ -0,0 +1,11 @@
+#Integer
+listX= [10,50,30,70]
+print(listX[1:5])
+
+#Float
+listY= [1.5,7.8,10.1]
+print(listY[1:5])
+
+#String
+listZ= ["Ralph","Bonilla", "Clerigo", "Balbin", "Andresa"]
+print(listZ[1:6])
