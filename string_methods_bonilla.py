20d0bf1e827cec8ca4bfba6e983e953bb9a86446
bonillaralph34 <45374698+bonillaralph34@users.noreply.github.com>
2019-02-19 11:14:22 +0800

    Create string_methods_bonilla.py

---
+++ string_methods_bonilla.py
@@ -0,0 +1,28 @@
+alphabet = "Ralph"
+x = alphabet.isalpha()
+print(x)
+
+capital = "ralph"
+
+y = capital.capitalize()
+print(y)
+
+case = "RALPH"
+
+z = case.casefold()
+print(z)
+
+cen = "RALPH"
+
+c = cen.center(50)
+print(c)
+
+spliting = "Welcome to DotA 2"
+
+s = spliting.split()
+print(s)
+
+replacing = input("Enter your word: ")
+
+a = replacing.replace("tangina", "*******")
+print(a)
